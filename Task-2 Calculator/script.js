56e26f013b4260dd418b3b6111413a37d1777252
Iqrash Jalil <iqrashjalil@gmail.com>
2024-02-24 22:18:59 +0500

    Task-2 Calculator
    write the JavaScript for the calculator.

---
+++ Task-2 Calculator/script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("display");
+
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+
+function clearDisplay(){
+    display.value = "";
+}
+
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(err){
+        display.value = "ERROR";
+    }
+}
